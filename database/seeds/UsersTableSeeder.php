ec71987c39f0a01020c6b54bf8493e04cba14135
Juan Navas <navascruzf@gmail.com>
2018-06-25 09:59:57 -0400

    first commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+   
+    public function run()
+    {
+        // User::create([
+        //     'name' => 'Juan',
+        //     'email' => 'juan@juannavas.com.ve',
+        //     'password' => bcrypt('123123')
+        // ]);
+    }
+}
